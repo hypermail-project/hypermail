25d5164ffd75ef7e0692b14c848f5ec07c0f5fd4
Jose Kahan <jose@w3.org>
2003-06-10 18:14:19 +0000

    JK: Applied patch suggested by PCM to remove the spurious Message-Id warning.

--- src/parse.c
+++ src/parse.c
@@ -2837,8 +2837,7 @@ int parse_old_html(int num, struct emailinfo *ep, int parse_body,
 		    char *raw_msgid = getvalue(line);
 		    msgid = unspamify(raw_msgid);
 		    if (raw_msgid) free(raw_msgid);
-		    if (msgid && (!isalnum(*msgid) || !strchr(msgid,'.')
-				  || !strstr(line,"-->")) && set_linkquotes)
+		    if (msgid && !strstr(line,"-->") && set_linkquotes)
 		        msgid = NULL;/* old version of Hypermail wrote junk? */
 		}
 		else if (!strcasecmp(command, "charset"))
