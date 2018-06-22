5df9ca9f4279e3f108a79892163b5133c46b03aa
jkbzh <jkgh@w3.org>
2018-06-22 13:39:12 +0200

    Rather than converting the content-type from multipart/alternative to multipart/mixed, use hypermail's set_save_alts to do the same in a cleaner way. The Apple Mail ua header value may change depening on what device is used. Changed set_applemail_ua_value to a list so that new ones can be added if needed. The default list values are "Apple iPhone" (concatenanted to "Mail (" during the check (

--- src/setup.h
+++ src/setup.h
@@ -99,7 +99,6 @@ extern bool set_use_sender_date;
 extern bool set_inline_addlink;
 extern bool set_applemail_mimehack;
 extern char *set_applemail_ua_header;
-extern char *set_applemail_ua_value;
 
 extern int set_showhtml;
 extern int set_thrdlevels;
@@ -146,6 +145,7 @@ extern struct hmlist *set_filter_out;
 extern struct hmlist *set_filter_require;
 extern struct hmlist *set_filter_out_full_body;
 extern struct hmlist *set_filter_require_full_body;
+extern struct hmlist *set_applemail_ua_value;
 
 extern bool set_format_flowed;
 extern bool set_format_flowed_disable_quoted;
