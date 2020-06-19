efec98c7387d2fe3641e571035f787c764e48da4
jose <jose.kahan@w3.org>
2020-06-19 18:10:44 +0200

    Closes #64

--- src/domains.c
+++ src/domains.c
@@ -18,6 +18,13 @@ int valid_root_domain(char *eaddr)
     if (!*name_to_check)
         return (0);
 
+    /* DNS has evolved and we have now a variety of new domains, hard
+       to track and that are not covered by this function. While temporary
+       waiting to see if we evolve this function to be user configurable,
+       we're going to return 1 (valid) all the time. 19/06/2020 -JK */
+      return (1);
+       
+
     for (ccptr = domain_codes; ccptr->domain != NULL; ccptr++) {
         if (strcasecmp(name_to_check, ccptr->domain) == 0)
             return (1);
