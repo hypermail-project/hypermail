54250968205592bb4504fe9c396fe3b2a90bb269
jose <jose.kahan@w3.org>
2021-10-11 19:12:49 +0200

    new function i18n_replace_unicode_spaces() Replaces all unicode spaces in a string by an ascii space

--- src/proto.h
+++ src/proto.h
@@ -104,7 +104,8 @@ char *unobfuscate_email_address (char *);
 char *i18n_convstring(char *, char *, char *, size_t *);
 char *i18n_utf2numref(char *, int);
 unsigned char *i18n_numref2utf(char *);
-
+int i18n_replace_unicode_spaces(char *, char *, size_t);
+  
 char *PushByte(struct Push *, char);
 char *PushString(struct Push *, const char *);
 char *PushNString(struct Push *, const char *, int);
