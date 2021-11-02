10d01384323476794d8fc8fe40eced7d262f41e5
jose <jose.kahan@w3.org>
2021-11-02 19:31:05 +0100

    add more context to identify when we're parsing message/rfc822 attachments

--- src/parse.c
+++ src/parse.c
@@ -2257,7 +2257,7 @@ int parsemail(char *mbox,	/* file name */
 			     */
 			    bp = addbody(bp, &lp,
 					 "<p><strong>attached mail follows:</strong></p><hr />",
-					 BODY_HTMLIZED | bodyflags);
+					 BODY_ATTACHMENT_RFC822 | BODY_HTMLIZED | bodyflags);
 			    bodyflags |= BODY_ATTACHED;
 			    /* @@ should it be 1 or 2 ?? should we use another method? */
 #if 0
