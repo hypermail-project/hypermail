70ef7f9b3b54eff393ca74803ac42434e9b21c9b
jkbzh <3439365+jkbzh@users.noreply.github.com>
2026-07-16 06:03:07 +0200

    cleaning autoupdate 2.72 warnings wrt HAVE_STDC_HEADERS, HAVE_TIME_H and HAVE_SYS_TIME_H

--- src/getdate.h
+++ src/getdate.h
@@ -26,17 +26,13 @@
 #  include <stdlib.h>
 # endif
 # include <sys/types.h>
-# ifdef TIME_WITH_SYS_TIME
+# ifdef HAVE_SYS_TIME_H
 #  include <sys/time.h>
-#  include <time.h>
-# else
-#  ifdef HAVE_SYS_TIME_H
-#   include <sys/time.h>
-#  else
-#   include <time.h>
-#  endif
 # endif
-#endif				/* defined (vms) */
+# ifdef HAVE_TIME_H
+#  include <time.h>
+# endif
+#endif /* defined (vms) */
 
 #ifdef NO_MACRO
 #undef isspace
