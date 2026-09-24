c5c3e6755ff029fa7f8bdd44c388c8b04972819d
jkbzh <3439365+jkbzh@users.noreply.github.com>
2026-09-24 23:02:03 +0200

    mdecodeRFC2047 wasn't being called on Content-Type header
    parser wasn't applying the RFC20247 mime decoder to
    Content-Type headers, affecting specifically the header's
    name attribute value.

--- src/parse.c
+++ src/parse.c
@@ -1070,6 +1070,8 @@ extract_rfc2047_content(char *iptr)
 ** Should result in "I'm called Daniel" too.
 **
 ** Returns the newly allcated string, or the previous if nothing changed
+** If charsetsave is not NULL, returns the charset used to encode
+** the string.
 */
 
 static char *mdecodeRFC2047(char *string, int length, char *charsetsave)
@@ -1147,9 +1149,11 @@ static char *mdecodeRFC2047(char *string, int length, char *charsetsave)
 		memcpy(output,output3,len);
 		output += len;
 		free(output3);
-		charsetlen = strlen(charset) < 255 ? strlen(charset) : 255;
-		memcpy(charsetsave,charset,charsetlen);
-		charsetsave[charsetlen] = '\0';
+                if (charsetsave) {
+                    charsetlen = strlen(charset) < 255 ? strlen(charset) : 255;
+                    memcpy(charsetsave,charset,charsetlen);
+                    charsetsave[charsetlen] = '\0';
+                }
 #else
 		for (; ptr < endptr; ptr++) {
 		    switch (*ptr) {
@@ -1180,9 +1184,11 @@ static char *mdecodeRFC2047(char *string, int length, char *charsetsave)
 		memcpy(output,output2,tmplen);
 		output += tmplen;
 		free(output2);
-		charsetlen = strlen(charset) < 255 ? strlen(charset) : 255;
-		memcpy(charsetsave,charset,charsetlen);
-		charsetsave[charsetlen] = '\0';
+                if (charsetsave) {
+                    charsetlen = strlen(charset) < 255 ? strlen(charset) : 255;
+                    memcpy(charsetsave,charset,charsetlen);
+                    charsetsave[charsetlen] = '\0';
+                }
 #else
                 int len;
                 
@@ -2306,7 +2312,16 @@ int parsemail(char *mbox,	/* file name */
                         continue;
                     
                     if (!strncasecmp(head->line, "Content-Type:", 13)) {
-			char *ptr = head->line + 13;
+                        char *ptr;
+
+                        if ( !head->demimed ) {
+                            /* we are not interested in charsets used to encode strings
+                               in this header */
+                            head->line =
+                                mdecodeRFC2047(head->line, strlen(head->line), NULL);
+                            head->demimed = TRUE;
+                        }
+                        ptr = head->line + 13;
 #define DISP_HREF 1
 #define DISP_IMG  2
 #define DISP_IGNORE 3
